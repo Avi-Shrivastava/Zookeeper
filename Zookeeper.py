e57a7d006578c0037616b0bafb9e3da1c1d7288b
Avi-Shrivastava <71298014+Avi-Shrivastava@users.noreply.github.com>
2021-12-16 11:56:08 -0500

    Add files via upload

--- Zookeeper.py
+++ Zookeeper.py
@@ -1,3 +1,29 @@
-import string
-alphabet =
-double_alphabet = {}
+# Part 1
+print(''' I love animals!
+Let's check on the animals...
+The deer looks fine.
+The bat looks happy.
+The lion looks healthy.''')
+
+# Part 2
+print(r"""
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!
+""")
